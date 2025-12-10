2795b279c204b5021321fb9f2da496d6fc5bb2bb
miteshmahalle <miteshmahalle@gmail.com>
2025-12-10 11:31:36 +0530

    string

--- Backend/config.py
+++ Backend/config.py
@@ -8,7 +8,8 @@ DB_CONFIG = {
     'host': os.getenv("DB_HOST"),
     'user': os.getenv("DB_USER"),
     'password': os.getenv("DB_PASSWORD"),
-    'database': os.getenv("DB_NAME")
+    'database': os.getenv("DB_NAME"),
+    'port': (os.getenv("DB_PORT"))
 }
 
 SECRET_KEY = os.getenv("SECRET_KEY")
